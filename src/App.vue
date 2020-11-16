4c4d3cd9eaa64e322d8f03b411a1fb2dfa8b68b2
sulei <287909077@qq.com>
2020-11-16 13:52:49 +0800

    salimsu

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import {Button} from 'vant'
+
+  export default {
+
+    name: 'App',
+    components: {
+      [Button.name]: Button
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
